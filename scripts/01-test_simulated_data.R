1e44c004ace59caa2df9efec285351a41ac2fe95
ClaireMa0311 <minglu.ma@mail.utoronto.ca>
2024-12-10 21:06:37 -0500

    update 01,04,06 scripts

--- scripts/01-test_simulated_data.R
+++ scripts/01-test_simulated_data.R
@@ -5,13 +5,14 @@
 # Contact: minglu.ma@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-  # - The `tidyverse` package must be installed and loaded
+  # - The `tidyverse` and `testthat` packages must be installed and loaded
   # - 00-simulate_data.R must have been run
 # Any other information needed? Make sure you are in the `starter_folder` rproj
 
 
 #### Workspace setup ####
 library(tidyverse)
+library(testthat)
 
 data <- read_csv("data/00-simulated_data/simulated_data.csv")
 
@@ -24,26 +25,22 @@ if (exists("data")) {
 
 
 #### Test data ####
+# Test if values in `ward` are greater than 0
+test_that("All values in 'ward' are greater than 0", {
+  expect_true(all(data$ward > 0))
+})
 
-# 1. Test if values in `ward` are greater than 0
-ward_test <- all(data$ward > 0)
-cat("Test 1: All values in 'ward' are greater than 0:", ward_test, "\n")
+# Test if values in `TOTSPACE` (Total Space) are greater than 0
+test_that("All values in 'TOTSPACE' are greater than 0", {
+  expect_true(all(data$TOTSPACE > 0))
+})
 
-# 2. Test if values in `TOTSPACE` (Total Space) are greater than 0
-totspace_test <- all(data$TOTSPACE > 0)
-cat("Test 2: All values in 'TOTSPACE' are greater than 0:", totspace_test, "\n")
+# Test if values in `subsidy` are either 0 or 1
+test_that("All values in 'subsidy' are either 0 or 1", {
+  expect_true(all(data$subsidy %in% c(0, 1)))
+})
 
-# 3. Test if values in `subsidy` are either 0 or 1
-subsidy_test <- all(data$subsidy %in% c(0, 1))
-cat("Test 3: All values in 'subsidy' are either 0 or 1:", subsidy_test, "\n")
-
-# 4. Test if values in `cwelcc_flag` are either 0 or 1
-cwelcc_flag_test <- all(data$cwelcc_flag %in% c(0, 1))
-cat("Test 4: All values in 'cwelcc_flag' are either 0 or 1:", cwelcc_flag_test, "\n")
-
-# Combine results
-if (ward_test & totspace_test & subsidy_test & cwelcc_flag_test) {
-  cat("All tests passed! The dataset meets the specified conditions.\n")
-} else {
-  cat("One or more tests failed. Please inspect the dataset for issues.\n")
-}
+# Test if values in `cwelcc_flag` are either 0 or 1
+test_that("All values in 'cwelcc_flag' are either 0 or 1", {
+  expect_true(all(data$cwelcc_flag %in% c(0, 1)))
+})
--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -5,13 +5,14 @@
 # Contact: minglu.ma@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-# - The `readr` package must be installed and loaded
+# - The `arrow` and `testthat` packages must be installed and loaded
 # - 03-clean_data.R must have been run
 # Any other information needed? Make sure you are in the `starter_folder` rproj
 
 #### Workspace setup ####
-library("readr")
-data <- read_csv("../data/02-analysis_data/cleaned.csv")
+library("arrow")
+
+data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
 
 
 #### Test data ####
@@ -24,26 +25,24 @@ if (exists("data")) {
 
 
 #### Test data ####
+# Test if values in `ward` are greater than 0
+test_that("All values in 'ward' are greater than 0", {
+  expect_true(all(data$ward > 0))
+})
 
-# 1. Test if values in `ward` are greater than 0
-ward_test <- all(data$ward > 0)
-cat("Test 1: All values in 'ward' are greater than 0:", ward_test, "\n")
+# Test if values in `TOTSPACE` (Total Space) are greater than 0
+test_that("All values in 'TOTSPACE' are greater than 0", {
+  expect_true(all(data$TOTSPACE > 0))
+})
 
-# 2. Test if values in `TOTSPACE` (Total Space) are greater than 0
-totspace_test <- all(data$TOTSPACE > 0)
-cat("Test 2: All values in 'TOTSPACE' are greater than 0:", totspace_test, "\n")
+# Test if values in `subsidy` are either 0 or 1
+test_that("All values in 'subsidy' are either 0 or 1", {
+  expect_true(all(data$subsidy %in% c(0, 1)))
+})
 
-# 3. Test if values in `subsidy` are either 0 or 1
-subsidy_test <- all(data$subsidy %in% c(0, 1))
-cat("Test 3: All values in 'subsidy' are either 0 or 1:", subsidy_test, "\n")
+# Test if values in `cwelcc_flag` are either 0 or 1
+test_that("All values in 'cwelcc_flag' are either 0 or 1", {
+  expect_true(all(data$cwelcc_flag %in% c(0, 1)))
+})
 
-# 4. Test if values in `cwelcc_flag` are either 0 or 1
-cwelcc_flag_test <- all(data$cwelcc_flag %in% c(0, 1))
-cat("Test 4: All values in 'cwelcc_flag' are either 0 or 1:", cwelcc_flag_test, "\n")
 
-# Combine results
-if (ward_test & totspace_test & subsidy_test & cwelcc_flag_test) {
-  cat("All tests passed! The dataset meets the specified conditions.\n")
-} else {
-  cat("One or more tests failed. Please inspect the dataset for issues.\n")
-}
